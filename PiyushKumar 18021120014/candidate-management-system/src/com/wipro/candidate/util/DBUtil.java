940bbe7567d4f41c91e271e62b357290641c4efb
Piyush Kumar <pkumar22122001@gmail.com>
2020-09-08 23:12:30 +0530

    Java Project

---
+++ PiyushKumar 18021120014/candidate-management-system/src/com/wipro/candidate/util/DBUtil.java
@@ -0,0 +1,22 @@
+package com.wipro.candidate.util;
+
+import java.sql.*;
+
+
+public class DBUtil {
+	public static Connection getDbConn(){
+		Connection conn = null;
+	      try {
+	         Class.forName("org.postgresql.Driver");
+	         conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/piyush", "postgres", "1204");
+	         System.out.println("Opened database successfully");
+	         return conn;
+	         
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	         System.err.println(e.getClass().getName()+": "+e.getMessage());
+	         System.exit(0);
+	      }
+		return conn;
+	}
+}
